ba2ec785e72e30f450d6074a3c9e3b539ad72d49
zoe-rowe8 <zoemarierowe@hotmail.co.uk>
2022-11-17 14:14:57 +0000

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+let myName = 'Zoe';
+document.write(myName);
+myName = 'Zoe Rowe';
+document.write(myName);
+
+/* let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize); */
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
