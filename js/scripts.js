3bd75697a92fadd477222fbddb2bbc5e686b70e4
zoe-rowe8 <zoemarierowe@hotmail.co.uk>
2022-11-18 13:47:35 +0000

    Adding Pokemon objects to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,37 @@
 let pokemonList = [
-    {}
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        type: ['Electric']
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        type: ['Water']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        type: ['Fire']
+    },
+    {
+        name: 'Pidgey',
+        height: 0.3,
+        type: ['Flying', 'Normal']
+    },
+    {
+        name: 'Persian',
+        height: 1,
+        type: ['Normal']
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        type: ['Fire', 'Flying']
+    },
+    {
+        name:'Jigglypuff',
+        height: 0.5,
+        type: ['Fairy', 'Normal']
+    }
 ];
